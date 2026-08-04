cfc5bfe29bbe1b1ba98e2de40125f92620c546a8
Ahmed-Manakhly <aelmnakhly@gmail.com>
2026-08-04 04:47:58 +0300

    feat: update social media icons to use direct external links for Facebook and LinkedIn

--- src/pages/About.js
+++ src/pages/About.js
@@ -70,9 +70,9 @@ function About() {
                             <p>CEO</p>
                         </div>
                         <div className={classes["team__mem__social"]}>
-                            <Link to="/" className={classes["team__social__link"]}>
+                            <a href="https://www.facebook.com/ahmed.abdul.rahman.834668" target="_blank" rel="noopener noreferrer" className={classes["team__social__link"]}>
                                 <ion-icon name="logo-facebook"></ion-icon>
-                            </Link>
+                            </a>
                             <Link to="/" className={classes["team__social__link"]}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
@@ -81,9 +81,9 @@ function About() {
                             <Link to="/" className={classes["team__social__link"]}>
                                 <ion-icon name="logo-instagram"></ion-icon>
                             </Link>
-                            <Link to="/" className={classes["team__social__link"]}>
+                            <a href="https://www.linkedin.com/in/ahmed-manakhly-6b9543219/" target="_blank" rel="noopener noreferrer" className={classes["team__social__link"]}>
                                 <ion-icon name="logo-linkedin"></ion-icon>
-                            </Link>
+                            </a>
                         </div>
                     </article>
             <article className={classes["team__mem"]}>
